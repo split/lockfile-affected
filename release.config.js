9b9437362a9f7e06f26034bdbf200b6bc9509aab
Henri Lahtinen <splitti@gmail.com>
2026-04-01 15:34:02 +0300

    fix: use pnpm version instead of npm in prepareCmd

--- release.config.js
+++ release.config.js
@@ -16,7 +16,7 @@ const config = {
     [
       '@semantic-release/exec',
       {
-        prepareCmd: `npm version \${nextRelease.version} --no-git-tag-version --message "chore(release): ${shortName} \${nextRelease.version} [skip ci]"`,
+        prepareCmd: `pnpm version \${nextRelease.version} --no-git-tag-version --message "chore(release): ${shortName} \${nextRelease.version} [skip ci]"`,
         publishCmd: 'pnpm publish --access public',
       },
     ],
